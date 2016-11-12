e6a16ccb44bb1f0f20e92c66ac470d2c3cc3bf42
Kevin Wang <kev.wang424@gmail.com>
2016-11-12 19:59:08 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
- # your code here!
+  array = []  #this is to push all trues into
+  idx = 0
+
+  while idx < collection.length
+    if yield(collection[idx]) == true
+      array << collection[idx]
+      idx += 1
+    else
+      idx += 1
+    end
+  end
+  array
 end
